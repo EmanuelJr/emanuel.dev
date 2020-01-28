0f577f16a59760095683cc990795259aa93c12ca
Emanuel Junior <emanuelmpjr@gmail.com>
2020-01-28 10:30:24 -0300

    fix(processor): use println to show message

--- src/processor.js
+++ src/processor.js
@@ -3,11 +3,13 @@ function processor(terminal, text) {
   const commandName = textSplited[0];
 
   const command = terminal.commands[commandName];
+
   if (!command) {
-    return `command not found: ${commandName}`;
+    terminal.println(`command not found: ${commandName}`);
+    return;
   }
 
-  return command.call(terminal, textSplited.slice(1));
+  command.call(terminal, textSplited.slice(1));
 }
 
 export default processor;
